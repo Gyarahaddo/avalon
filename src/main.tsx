52907c33ff2fe67d8c02d0696784de31ddf4b7dd
Gyarahaddo <galahad@fastmail.com>
2025-11-12 21:44:58 +0800

    update deploy job to AWS S3 and remove deprecated code

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@avalon/globally.css";
 import Home from "@avalon/Home";
-// import App from "@avalon/App.tsx";
-// import { MyButton } from "@avalon/components/buttons.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
